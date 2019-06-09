23bb52c5b4ebe636ca11e5bc052f81f55d932097
Cameron Clough <cameronjclough@gmail.com>
2019-06-09 02:05:32 +0100

    feat: add registerName function to Twooter Client

--- lib/src/twooter_client.dart
+++ lib/src/twooter_client.dart
@@ -1,13 +1,20 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// The official Twooter client for Dart!
+import 'package:dio/dio.dart';
+
+import 'message.dart';
+
+// The unofficial Twooter client for Dart!
 //
 // TODO: add features to match official Java client
 class TwooterClient {
   static final String _TWOOTER_API_URL = 'http://twooter.johnvidler.co.uk';
+  Dio _dio;
 
   // Creates a new client instance
-  TwooterClient();
+  TwooterClient() {
+    this._dio = Dio();
+  }
 
   // Attempts to determine if the web service is both *online* and *reachable*.
   Future<bool> isUp() async {
@@ -15,6 +22,38 @@ class TwooterClient {
     return response.statusCode == 200 && response.body == 'OK';
   }
 
-  Future<http.Response> _query(String path, {dynamic body}) async =>
-      await http.post(_TWOOTER_API_URL + path, body: body);
+  // Attempt to register a new username.
+  Future<String> registerName(String name) async {
+    var response = await _query('/registerName', body: {'name': name});
+    return response.body;
+  }
+
+  Future<HTTPResponse> _query(String path, {Map body}) async {
+    var response;
+
+    if (body == null) {
+      // make a basic post request when no data is provided
+      response = await _dio.post(_TWOOTER_API_URL + path);
+    } else {
+      // make a post request with json encoded data
+      response = await _dio.post(
+        _TWOOTER_API_URL + path,
+        data: jsonEncode(body),
+        options: Options(
+          validateStatus: (status) => true,
+        ),
+      );
+    }
+
+    // return relevant data
+    // TODO: encode JSON response
+    return HTTPResponse(response.statusCode, response.data);
+  }
+}
+
+class HTTPResponse {
+  final int statusCode;
+  final dynamic body;
+
+  HTTPResponse(this.statusCode, this.body);
 }
--- test/twooter_dart_test.dart
+++ test/twooter_dart_test.dart
@@ -1,5 +1,19 @@
 import 'package:twooter_dart/twooter_dart.dart';
 import 'package:test/test.dart';
+import 'dart:math';
+
+String _randomUsername({length = 10}) {
+  final rng = Random();
+  final characters = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  // create 12 char string
+  final charCodes = new List.generate(length, (index) {
+    int n = rng.nextInt(characters.length);
+    return characters.codeUnitAt(n);
+  });
+
+  return String.fromCharCodes(charCodes);
+}
 
 void main() {
   group('A group of tests', () {
@@ -12,6 +26,12 @@ void main() {
     test('First Test', () async {
       final value = await client.isUp();
       expect(value, isTrue);
+    });
+
+    test('Register Random Name', () async {
+      final username = 'flutter-app-' + _randomUsername(length: 5);
+      final value = await client.registerName(username);
+      expect(value, isNotNull);
     });
   });
 }
